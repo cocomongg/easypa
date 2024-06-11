84db2f6161fc092039cebea3e6c7e1bea11aec06
cocomongg <pjm4142@naver.com>
2024-06-12 00:02:14 +0900

    init

---
+++ src/test/java/com/easypa/pa/PaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easypa.pa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
